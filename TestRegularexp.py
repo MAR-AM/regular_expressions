72fe1f2edb7ac4f5146a052819bbb8fc90005fd7
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-12-14 01:27:54 +0100

    THE TEST OF METHODS (✿◡‿◡)

---
+++ TestRegularexp.py
@@ -0,0 +1,17 @@
+from ExpressionsRégulières import *
+
+chaine="i am here for help ."
+word="me"
+print(recherche(chaine,word))
+
+digit_test="hello i am a student"
+print(contient_chiffres(digit_test))
+
+space_test="how are you"
+print(remplace(space_test))
+
+email_test = "exemple@domaine.com"
+print(email(email_test))
+
+numero_test = "12-34567890"
+print(telephone(numero_test))
